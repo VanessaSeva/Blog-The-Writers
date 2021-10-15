8b2692d56dbdddf11ec41ca8db49c0f78776b4b6
Vanessa Seva <vanessa.seva@fabrick.com>
2021-10-15 14:41:39 +0200

    mongoDB connexion

---
+++ back/app.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://vanessa:canislupus@cluster0.k6owq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
